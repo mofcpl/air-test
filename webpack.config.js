e0c51d73243a53de3f4d8ea17ddf9f7467811229
Dominik Zbróg <47178239+mofcpl@users.noreply.github.com>
2019-09-21 15:32:36 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports =
+{
+    module:
+    {
+        rules:[
+            {
+                test: /\.html$/,
+                use: [{loader: "html-loader", options: {minimize: true}}]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {loader: "babel-loader"}
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [ 'style-loader', 
+                    MiniCssExtractPlugin.loader, 
+                    {
+                      loader: 'css-loader'
+                    },
+                    { loader: 'postcss-loader', options: { config: { path: 'src/postcss-config/' }}},
+                    'sass-loader']
+            },
+
+            {
+                test: /\.(png)$/, 
+                use: [
+                {
+                  loader: "url-loader",
+                  options: {
+                    name: "./img/[name].[ext]",
+                    limit: 10000
+                  }
+                },
+          {
+            loader: "img-loader"
+          }
+        ]
+            },
+            {
+              test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+              loader: 'url-loader',
+              options: {
+                limit: 4096,
+                name: './fonts/[name].[ext]?[hash]', // was '/fonts/[name].[ext]?[hash]',
+              },
+            },
+        ]
+    },
+    
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
