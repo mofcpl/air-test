b63c3e530480dfaa1222c1e000871cbb70dc84e9
Dominik Z <zbrogdom@gmail.com>
2022-12-28 19:27:29 +0100

    change class to components, change to hooks

--- src/index.js
+++ src/index.js
@@ -1,43 +1,101 @@
 import style from "./main.scss";
 import "./css/fontello.css"
 
-import React, { Component } from "react";
+import React, { useEffect, useReducer } from "react";
 import ReactDOM from 'react-dom/client';
-import { Provider, connect } from 'react-redux'
 
 import {Summary, Data, Station, Button, Title, Desc} from "./interface.jsx"
-import {store, setPosition, setClosestStation, setSensors, setSummary, reset, setIndexes, setStatus, setData} from "./redux-store.js"
 
 require("babel-polyfill");
 
-class App extends React.Component
+const defaultState =
 {
-    constructor(props)
+    status: "START",
+    position:
     {
-        super(props);
+        latitude: 0,
+        longitude: 0
+    },
+    closestStation: 
+    {
+        index: "",
+        name: "",
+        distance: ""
+    },
+    data: [],
+    summary:
+    {
+        quality: "",
+        date: "",
+        id: ""
+    }
+}
 
-        this.state =
-        {
-            ticks: 0
+const appStateReducer = (state, action) => {
+    switch(action.type)
+    {
+        case 'RESET':                 return defaultState;
+        case 'SET_POSITION':          return Object.assign({},state, {position: action.value});
+        case 'SET_CLOSEST_STATION':   return Object.assign({},state, {closestStation: action.value});
+        case 'SET_SUMMARY':           return Object.assign({},state, {summary: action.value});
+        case 'SET_STATUS':            return Object.assign({},state, {status: action.value});
+        case 'SET_SENSORS':
+        return{
+            ...state,
+            data: [...state.data, action.value]
+        }   
+        case 'SET_INDEXES':
+        return{
+            ...state,
+            data: state.data.map((currentValue, index) => (index === action.index) ? {...currentValue, index: action.value, id: action.value2} : currentValue)
         }
-
-        this.loadPosition =         this.loadPosition.bind(this);
-        this.checkAir =             this.checkAir.bind(this);
-        this.findNearestStation =   this.findNearestStation.bind(this);
-        this.loadSummary =          this.loadSummary.bind(this);
-        this.calcDistance =         this.calcDistance.bind(this);
-        this.loadSensors =          this.loadSensors.bind(this);
-        this.loadData =             this.loadData.bind(this);
-        this.addTick =              this.addTick.bind(this);
+        case 'SET_DATA':
+        return{
+            ...state,
+            data: state.data.map((currentValue, index) => (index === action.index) ? {...currentValue, value: action.value} : currentValue)
+        }
+        default: return defaultState;
     }
+}
 
-    addTick()
-    {
-        this.setState({ticks: this.state.ticks + 1});
-    }
+// START -> POSITION -> NEAREST_STATION -> SENSORS -> DATA -> SUMMARY -> READY
+const App = () =>
+{
+    const [appState, dispatchState] = useReducer(appStateReducer, defaultState)
+
+    useEffect(() => {
+        switch(appState.status)
+        {
+            // START -> POSITION -> NEAREST_STATION -> SENSORS -> DATA -> SUMMARY -> READY
+            case "POSITION":
+            {
+                dispatchState({type: 'SET_STATUS', value: "LOADING"});
+                findNearestStation();
+                break;
+            }
+            case "NEAREST_STATION":
+            {
+                dispatchState({type: 'SET_STATUS', value: "LOADING"});
+                loadSensors();
+                break;
+            }
+            case "SENSORS":
+            {
+                dispatchState({type: 'SET_STATUS', value: "LOADING"});
+                loadData();
+                break;
+            }
+            case "DATA":
+            {
+                dispatchState({type: 'SET_STATUS', value: "LOADING"});
+                loadSummary();
+                break;
+            }
+        }
+    },[appState.status]);
 
     //https://stackoverflow.com/a/1502821
-    calcDistance(pos1, pos2)
+    const calcDistance = (pos1, pos2) =>
     {
         const degToRad = (x) =>
         {
@@ -56,23 +114,28 @@ class App extends React.Component
         return d; // returns the distance in meter
     }
 
-    loadPosition()
+    const loadPosition = () =>
     {        
         console.log("calculating position...");
         if (navigator.geolocation)
         {
-          navigator.geolocation.getCurrentPosition(position => 
-          {
-            this.props.submitSetPosition({latitude: position.coords.latitude, longitude: position.coords.longitude});
-            //this.props.submitSetPosition({latitude: 50.874167, longitude: 20.633333});
-            console.log("calculating position...done");
-            this.props.submitSetStatus("POSITION");
-          });
+            navigator.geolocation.getCurrentPosition(position => 
+            {
+                dispatchState({
+                    type: 'SET_POSITION', 
+                    value: {
+                        latitude: position.coords.latitude, 
+                        longitude: position.coords.longitude
+                    }});
+                //this.props.submitSetPosition({latitude: 50.874167, longitude: 20.633333});
+                console.log("calculating position...done");
+                dispatchState({type: 'SET_STATUS', value: "POSITION"});
+            });
         }
         
     }
     
-    async findNearestStation()
+    const findNearestStation = async () =>
     {
         console.log("finding nearest station...");
         const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/station/findAll")
@@ -83,15 +146,15 @@ class App extends React.Component
         let closestStationIndex = 0;
         let closestDistance = 0;
 
-        const myLat = this.props.state.position.latitude;
-        const myLng = this.props.state.position.longitude;
+        const myLat = appState.position.latitude;
+        const myLng = appState.position.longitude;
 
         jsonData.forEach( (element, index, array) =>
         {
             const newLat = element.gegrLat;
             const newLng = element.gegrLon;
             
-            const newDistance = this.calcDistance({lat: myLat, lng: myLng},{lat: newLat, lng: newLng});
+            const newDistance = calcDistance({lat: myLat, lng: myLng},{lat: newLat, lng: newLng});
 
             if(index == 0)
             {
@@ -112,19 +175,18 @@ class App extends React.Component
         const index = jsonData[closestStationIndex].id;
         const distance = closestDistance;
 
-        this.props.submitSetClosestStation({index, name, distance});
-        
-
-
+        dispatchState({
+            type: 'SET_CLOSEST_STATION', 
+            value: { index, name, distance }
+        });
         console.log("finding nearest station...done");
-        this.props.submitSetStatus("NEAREST_STATION");
-
+        dispatchState({type: 'SET_STATUS', value: "NEAREST_STATION"});
     }
 
-    async loadSensors()
+    const loadSensors = async () =>
     {
         console.log("loading sensors from station...");
-        const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/station/sensors/"+this.props.state.closestStation.index)
+        const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/station/sensors/"+appState.closestStation.index)
         const jsonData = await response.json();
 
         console.log(jsonData);
@@ -132,19 +194,25 @@ class App extends React.Component
         jsonData.forEach( (element, index, array) =>
         {
             const tempName = jsonData[index].param.paramCode.toLowerCase().replace('.',"");
-            this.props.submitSetSensors({id: jsonData[index].id, name: jsonData[index].param.paramName, code: tempName});
+            dispatchState({
+                type: 'SET_SENSORS', 
+                value: {
+                    id: jsonData[index].id, 
+                    name: jsonData[index].param.paramName, 
+                    code: tempName
+                }});
         });
 
         console.log("loading sensors from station...done");
-        this.props.submitSetStatus("SENSORS");
+        dispatchState({type: 'SET_STATUS', value: "SENSORS"});
 
     }
 
-    async loadData()
+    const loadData = () =>
     {
         console.log("loading data from station...");
         let number = 0;
-        this.props.state.data.forEach( async (element, index, array) =>
+        appState.data.forEach( async (element, index, array) =>
         {
             const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/data/getData/"+ element.id)
             const jsonData = await response.json();
@@ -159,137 +227,71 @@ class App extends React.Component
                 j++;
             }
 
-            this.props.submitSetData(index, jsonData.values[j].value);
+            dispatchState({
+                type: 'SET_DATA', 
+                value: jsonData.values[j].value,
+                index
+            });
 
             if(number >= array.length)
             {
                 console.log("loading data from station...done");
-                this.props.submitSetStatus("DATA");
+                dispatchState({type: 'SET_STATUS', value: "DATA"});
             }
 
         })
     }
 
-    async loadSummary()
+    const loadSummary = async () =>
     {
         console.log("loading summary from station...");
-        const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/"+this.props.state.closestStation.index)
+        const response = await fetch("https://cors-anywhere.herokuapp.com/"+"http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/"+appState.closestStation.index)
         const jsonData = await response.json();
 
         console.log(jsonData);
-        for(let i = 0; i < this.props.state.data.length; i++)
+        for(let i = 0; i < appState.data.length; i++)
         {
-            const tempName = this.props.state.data[i].code+'IndexLevel';
+            const tempName = appState.data[i].code+'IndexLevel';
             if(!jsonData[tempName]) continue;
-            this.props.submitSetIndexes(i,jsonData[tempName].indexLevelName, jsonData[tempName].id);
+            dispatchState({
+                type: 'SET_INDEXES', 
+                index: i,
+                value: jsonData[tempName].indexLevelName, 
+                value2: jsonData[tempName].id
+            });
         }
-        this.props.submitSetSummary({quality: jsonData.stIndexLevel.indexLevelName, date: jsonData.stSourceDataDate, id: jsonData.stIndexLevel.id});
+        dispatchState({
+            type: 'SET_SUMMARY', 
+            value: {
+                quality: jsonData.stIndexLevel.indexLevelName, 
+                date: jsonData.stSourceDataDate, 
+                id: jsonData.stIndexLevel.id
+            }});
 
         console.log("loading summary from station...done");
-        this.props.submitSetStatus("SUMMARY");   
+        dispatchState({type: 'SET_STATUS', value: "SUMMARY"}); 
 
     }
 
-    componentDidUpdate(prevProps)
-    {      
-        
-        switch(this.props.state.status)
-        {
-            // START -> POSITION -> NEAREST_STATION -> SENSORS -> DATA -> SUMMARY -> READY
-            case "POSITION":
-            {
-                this.props.submitSetStatus("LOADING");
-                this.findNearestStation();
-                break;
-            }
-            case "NEAREST_STATION":
-            {
-                this.props.submitSetStatus("LOADING");
-                this.loadSensors();
-                break;
-            }
-            case "SENSORS":
-            {
-                this.props.submitSetStatus("LOADING");
-                this.loadData();
-                break;
-            }
-            case "DATA":
-            {
-                this.props.submitSetStatus("LOADING");
-                this.loadSummary();
-                break;
-            }
-        }
-        
-    }
-
-    checkAir()
+    const checkAir = () =>
     {
         const promise = new Promise((resolve => resolve()));
-
-        promise.then(() => this.props.submitReset()).then( () => this.loadPosition());
-    
+        promise.then(() => dispatchState('RESET')).then( () => loadPosition());
     }
 
-    componentDidMount()
-    {
-        
-    }
-
-    render()
-    {
-    
-
-        return(
-        <main class="container">
-
-            <Title />
-            <Station data={this.props.state} />
-            <Data data={this.props.state} />
-            <Summary data={this.props.state} />
-            <Button checkAir={this.checkAir} />
-            <Desc data={this.props.state} />
-
-        </main>
-        );
-
-    }
+    return(
+    <main className="container">
+        <Title />
+        <Station data={appState} />
+        <Data data={appState} />
+        <Summary data={appState} />
+        <Button checkAir={checkAir} />
+        <Desc data={appState} />
+    </main>
+    );
 }
-
-const mapStateToProps = (state) => 
-{
-    return {state};
-};
-
-const mapDispatchToProps = (dispatch) => 
-{
-    return{
-        submitSetPosition: (value) =>           {dispatch(setPosition(value))},
-        submitSetClosestStation: (value) =>     {dispatch(setClosestStation(value))},
-        submitSetSensors: (value) =>{dispatch(setSensors(value))},
-        submitSetSummary: (value) =>            {dispatch(setSummary(value))},
-        submitReset: () =>                      {dispatch(reset())},
-        submitSetIndexes: (index, value, value2) =>     {dispatch(setIndexes(index, value, value2))},
-        submitSetStatus: (value) =>             {dispatch(setStatus(value))},
-        submitSetData: (index, value) =>   {dispatch(setData(index, value))}
-    }
-}
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(App);
-
-class AppWrapper extends React.Component 
-{
-    render() {
-      return (
-        <Provider store={store}>
-          <Container/>
-        </Provider>
-      );
-    }
-};
 
 const root = ReactDOM.createRoot(document.getElementById('App'));
-root.render(<AppWrapper />);
+root.render(<App />);
 
 
